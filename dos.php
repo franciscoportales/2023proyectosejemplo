e6b5fe2bee46973cedc7356e2d0e0918fc3a3517
Francisco Portales <franciscoportales@gmail.com>
2023-09-19 20:23:12 -0600

    first commit

---
+++ dos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dos</title>
+</head>
+
+<body>
+    <h1>Dos</h1>
+    <ul>
+        <li>Uno</li>
+        <li>Dos</li>
+        <li>Tres</li>
+    </ul>
+
+    <a href="index.php"></a>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+
+<body>
+    <h1>Ejemplo Git y GitHub</h1>
+    <p>
+        fecha: 19/09/2023
+    </p>
+
+    <a href="dos.php"></a>
+</body>
+
+</html>
